10f055516254cecaac81c3ed4fde7100637a6a86
Gregg Kellogg <gregg@kellogg-assoc.com>
2011-03-16 10:07:06 -0700

    Updated to output a listed set or all *-manifest.rdf files from manfest.ttl.

--- merged2variant.rb
+++ merged2variant.rb
@@ -1,38 +1,68 @@
-#/usr/bin/env ruby
-# Usage merged-to-variant [html4|html5|xhtml|svg]
+#!/usr/bin/env ruby
+# Re-creates host-language specific manifests from a merged manifest
+#
+# Usage merged-to-variant [html4|html5|xhtml|svgtiny]
+#
+# Without an argument, it re-creates all manifests.
 require 'linkeddata'
+require 'getoptlong'
 
-HOST_LANGUAGE = RDF::URI("http://www.w3.org/2006/03/test-description#hostLanguage")
-variant = ARGV[0] || "xhtml"
+save_manifests = false
 
-manifests = {
-  "html4" => RDF::URI("http://rdfa.digitalbazaar.com/test-suite/html4-manifest"),
-  "html5" => RDF::URI("http://rdfa.digitalbazaar.com/test-suite/html5-manifest"),
-  "svg" =>   RDF::URI("http://rdfa.digitalbazaar.com/test-suite/svgtiny-manifest"),
-  "xhtml" => RDF::URI("http://rdfa.digitalbazaar.com/test-suite/xhtml-manifest"),
-}
+opts = GetoptLong.new(
+  ["--save", GetoptLong::NO_ARGUMENT],
+)
+opts.each do |opt, arg|
+  case opt
+  when '--save' then save_manifests = true
+  end
+end
 
+TD = RDF::Vocabulary.new("http://www.w3.org/2006/03/test-description#")
+  
 r = RDF::N3::Reader.new(File.open("manifest.ttl"))
 g = RDF::Graph.new << r
-subjects = g.query(:object => manifests[variant]).to_a.map(&:subject).uniq
-#puts "subjects for #{variant}: #{subjects.inspect}"
 
-puts RDF::RDFXML::Writer.buffer(
-  :prefixes => r.prefixes,
-  :base_uri => "http://rdfa.digitalbazaar.com/test-suite/") { |w|
+# Extract specific host language manifest URIs from test:hostLanguage
+manifests = {}
+g.query(:predicate => TD.hostLanguage) do |stmt|
+  hl = stmt.object.to_s.split("/").last.sub("-manifest", "")
+  manifests[hl] ||= stmt.object
+end
 
-  g.each_triple do |s, p, o|
-    next if !subjects.include?(s) || p == HOST_LANGUAGE
-    s = RDF::URI(s.to_s.sub("test-cases", "test-cases/#{variant}"))
-    if o.uri?
-      o = RDF::URI(o.to_s.sub("test-cases", "test-cases/#{variant}"))
-      case variant
-      when 'xhtml'
-        o = RDF::URI(o.to_s.sub(/\.html$/, ".xhtml"))
-      when 'svg'
-        o = RDF::URI(o.to_s.sub(/\.html$/, ".svg"))
-      end
-    end
-    w << [s, p, o]
+# For each variant in the argument list, or for all detected variants
+variants = ARGV
+variants = manifests.keys if variants.empty?
+
+variants.each do |variant|
+  subjects = g.query(:object => manifests[variant]).to_a.map(&:subject).uniq
+
+  out = if save_manifests
+    fn = manifests[variant].to_s.split("/").last + ".rdf"
+    puts "Write manifest for #{variant} to #{fn}"
+    File.open(fn, "wb")
+  else
+    puts "\nManifest for #{variant}:\n"
+    STDOUT
   end
-}
+
+  RDF::RDFXML::Writer.new(out,
+    :prefixes => r.prefixes,
+    :base_uri => "http://rdfa.digitalbazaar.com/test-suite/") { |w|
+
+    g.each_triple do |s, p, o|
+      next if !subjects.include?(s) || p == TD.hostLanguage
+      s = RDF::URI(s.to_s.sub("test-cases", "test-cases/#{variant}"))
+      if o.uri?
+        o = RDF::URI(o.to_s.sub("test-cases", "test-cases/#{variant}"))
+        case variant
+        when 'xhtml'
+          o = RDF::URI(o.to_s.sub(/\.html$/, ".xhtml"))
+        when 'svgtiny'
+          o = RDF::URI(o.to_s.sub(/\.html$/, ".svg"))
+        end
+      end
+      w << [s, p, o]
+    end
+  }
+end
